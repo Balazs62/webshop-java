45ede4c2076778a5c6fcf36270adcd7c8b01d978
BalazsM <lengyelbm@gmail.com>
2026-03-31 15:39:44 +0200

    Load database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,21 @@
+INSERT INTO products (id, name, description, baseprice) 
+VALUES (1, 'Futócipő', 'Profi futócipő aszfaltra', 29900)
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO products (id, name, description, baseprice) 
+VALUES (2, 'Póló', 'Pamut sportpóló', 5900)
+ON CONFLICT (id) DO NOTHING;
+
+
+
+INSERT INTO product_variants (id, product_id, size, stock_quantity, color) 
+VALUES (1, 1, 'S', 10, 'fekete') 
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO product_variants (id, product_id, size, stock_quantity, color) 
+VALUES (2, 1, 'M', 20, 'kék') 
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO product_variants (id, product_id, size, stock_quantity, color) 
+VALUES (3, 1, 'L', 5, 'zöld') 
+ON CONFLICT (id) DO NOTHING;
