ea3b3896c842c34b8801d4b87fa4f26ac984ecdf
BalazsM <lengyelbm@gmail.com>
2026-03-31 17:04:15 +0200

    Try fix database

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,20 +6,18 @@ INSERT INTO products (id, name, description, baseprice)
 VALUES (2, 'Póló', 'Pamut sportpóló', 5900)
 ON CONFLICT (id) DO NOTHING;
 
-
-
 INSERT INTO product_variants (id, product_id, size, stock_quantity, color) 
-VALUES (1, 1, 'S', 10, 'fekete') S
+VALUES (1, 2, 'S', 10, 'fekete')
 ON CONFLICT (id) DO NOTHING;
 
 INSERT INTO product_variants (id, product_id, size, stock_quantity, color) 
-VALUES (2, 1, 'M', 20, 'kék') 
+VALUES (2, 2, 'M', 20, 'kék') 
 ON CONFLICT (id) DO NOTHING;
 
 INSERT INTO product_variants (id, product_id, size, stock_quantity, color) 
-VALUES (3, 1, 'L', 5, 'zöld') 
+VALUES (3, 2, 'L', 5, 'zöld') 
 ON CONFLICT (id) DO NOTHING;
 
 INSERT INTO product_variants (id, product_id, size, stock_quantity, color)
-VALUES (1, 2, 38, 10, 'fekete')
+VALUES (4, 1, 38, 10, 'fekete')
 ON CONFLICT (id) DO NOTHING;
