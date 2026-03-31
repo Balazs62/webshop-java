b7fdb43105ee29038103ff2274067dba7b533c03
BalazsM <lengyelbm@gmail.com>
2026-03-31 16:17:37 +0200

    Load database

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
+# 1. fázis: Építés Maven-nel
 FROM maven:3.9.6-eclipse-temurin-21 AS build
 COPY . .
 RUN mvn clean package -DskipTests
 
+# 2. fázis: Futtatás
 FROM eclipse-temurin:21-jdk
 COPY --from=build /target/*.war app.war
 EXPOSE 8080
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,7 +9,7 @@ ON CONFLICT (id) DO NOTHING;
 
 
 INSERT INTO product_variants (id, product_id, size, stock_quantity, color) 
-VALUES (1, 1, 'S', 10, 'fekete') 
+VALUES (1, 1, 'S', 10, 'fekete') S
 ON CONFLICT (id) DO NOTHING;
 
 INSERT INTO product_variants (id, product_id, size, stock_quantity, color) 
@@ -18,4 +18,8 @@ ON CONFLICT (id) DO NOTHING;
 
 INSERT INTO product_variants (id, product_id, size, stock_quantity, color) 
 VALUES (3, 1, 'L', 5, 'zöld') 
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO product_variants (id, product_id, size, stock_quantity, color)
+VALUES (1, 2, 38, 10, 'fekete')
 ON CONFLICT (id) DO NOTHING;
